3ea9bca99138bde26d5f063cd15d0b7aa37de5a5
Martin Vokrinek <martinvokrinek@Martins-MacBook-Air.local>
2022-05-14 17:55:10 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,67 @@
+const puppeteer = require('puppeteer');
+const express = require('express');
+var cors = require('cors')
+
+const app = express()
+app.use(cors())
+const port = 3000
+
+const players = [
+      {
+        id: 81386432,
+          name: "Martin",
+          handicap: 22,
+      },
+      {
+          id: 7065211,
+          name: "Ondra",
+          handicap: 0,
+      }
+      ,
+      {
+          id: 22199054,
+          name: "Adam",
+          handicap: 18,
+      }
+  ]
+
+async function fetchData() {
+const browser = await puppeteer.launch();
+
+  async function fetchForPlayer(id) {
+    const page = await browser.newPage();
+    await page.goto(`https://veloviewer.com/athlete/${id}`);
+    console.log('parsing...')
+    await page.waitForSelector('#explorerDD', { timeout: 1000 });
+
+    const div = await page.evaluate(() => {
+      return document.querySelector('#explorerDD').innerHTML;
+    });
+
+    const pattern = /\d+x\d+/
+    
+    const [matchStr] = div.match(pattern)
+    return parseInt(matchStr.split('x')[0])
+  }
+
+  try {
+
+    const results =  await Promise.all(players.map(async (player) => {
+      const score = await fetchForPlayer(player.id)
+      const total = score + player.handicap
+      return {...player, score, total }
+    }  ))
+    await browser.close();
+    return results
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+app.get('/', async (req, res) => {
+  res.json(await fetchData())
+})
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
